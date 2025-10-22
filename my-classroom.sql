55015a866d278afb11398cf6a270de344879999e
JubaStriker <jubastriker@gmail.com>
2025-10-22 22:53:19 +0600

    Table creation completed

--- my-classroom.sql
+++ my-classroom.sql
@@ -1,3 +1,19 @@
-CREATE DATABASE temp1;
-
 create database college;
+USE college;
+
+CREATE TABLE student (
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT NOT NULL
+);
+
+INSERT INTO student 
+(id, name, age)
+VALUES
+(1, "JUBIAR", 24),
+(2, "MAHI", 25);
+
+SELECT * FROM student;
+
+SHOW TABLES;
+
